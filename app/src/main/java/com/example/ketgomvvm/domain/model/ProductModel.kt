b9cac773226dac80303f14f4fd4836c0ba5e6196
suleyman <suleyman.basaranoglu@gmail.co>
2022-02-10 00:43:23 +0300

    Changed folder structure

---
+++ app/src/main/java/com/example/ketgomvvm/domain/model/ProductModel.kt
@@ -0,0 +1,40 @@
+package com.example.ketgomvvm.data.model
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "products_table")
+data class ProductModel(
+
+    @ColumnInfo(name = "productName")
+    val productName: String? = null,
+
+    @ColumnInfo(name = "productPrice")
+    val productPrice: Int? = null,
+
+    @ColumnInfo(name = "productDescription")
+    val productDescription: String? = null,
+
+    @ColumnInfo(name = "productStatus")
+    val productStatus: String? = null,
+
+    @ColumnInfo(name = "sellingLocation")
+    val sellingLocation: String? = null,
+
+    @ColumnInfo(name = "productImage")
+    val productImage: String? = null,
+
+    @ColumnInfo(name = "createdDate")
+    val createdDate: String? = null,
+
+    @ColumnInfo(name = "upCount")
+    var upCount: Int? = 0,
+
+    @ColumnInfo(name = "isSold")
+    var isSold: Boolean? = false,
+    @PrimaryKey(autoGenerate = true) val id: Int? = 0
+) : Parcelable
---
+++ app/src/main/java/com/example/ketgomvvm/utils/datastore/ProductManager.kt
@@ -0,0 +1,32 @@
+package com.example.ketgomvvm.presentation.datastore
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+private val Context.datastore: DataStore<Preferences> by preferencesDataStore("products")
+
+class ProductManager(val context: Context) {
+
+    private val myDatastore: DataStore<Preferences> = context.datastore
+
+    companion object {
+        val SELLING_LOCATION = stringPreferencesKey("SELLING_LOCATION")
+     }
+
+    suspend fun storeProductSellingData(sellingLocation: String) {
+        myDatastore.edit { preferences ->
+            preferences[SELLING_LOCATION] = sellingLocation
+        }
+    }
+
+    val productSellingLocationFlow: Flow<String> = myDatastore.data.map {
+        it[SELLING_LOCATION] ?: ""
+    }
+
+}
